d478157888dbe674dea5f670806d317d33335922
Monica Bacatan <monicab3002@outlook.com>
2023-01-12 12:51:51 -0800

    Fixed url to render app

--- public/js/script.js
+++ public/js/script.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
 		const message = $('#chatbot-input').val();
 		$.ajax({
 			type: 'POST',
-			url: 'http://localhost:3000/chatbot',	// Uncomment this when running locally
-			// url: 'https://comp4537-lab2-monb.onrender.com/chatbot',
+			// url: 'http://localhost:3000/chatbot',	// Uncomment this when running locally
+			url: 'https://comp4537-lab2-monb.onrender.com/chatbot',
 			data: {
 				message: message
 			},
